9b79a5b365f020e2dab94ae06e4400770084ecce
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-02-04 14:45:31 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+ vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("associate_filetype"),
+    pattern = { "python" },
+    callback = function()
+      vim.opt.shiftwidth = 4
+      vim.opt.formatoptions:remove({ "o" }) -- 防止使用 o 切换到下一行的时候自动加上注释符号(在上一行是注释的情况下)
+    end,
+  })
