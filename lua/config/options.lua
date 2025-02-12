cd5492e11ca23e2bba47a5196dc30de65d2405cb
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-02-12 14:53:03 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,36 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
-
 local opt = vim.opt
 
+vim.g.autoformat = false
 opt.clipboard = ""
 opt.wrap = true
 opt.relativenumber = false
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+
+  -- 设置标签颜色
+  vim.g.neovide_title_background_color =
+    string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+  vim.g.neovide_title_text_color = "pink"
+
+  -- 其他设置
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12" -- neovide字体及其字体大小
+  vim.g.neovide_scale_factor = 1.0 -- 界面字体缩放大小
+  vim.g.neovide_floating_blur_amount_x = 2 -- 浮动窗口的模糊程度
+  vim.g.neovide_floating_blur_amount_y = 2 -- 浮动窗口的模糊程度
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_transparency = 1  -- 透明程度，作用于整个窗口
+  vim.g.neovide_normal_opacity = 1  -- 透明程度，仅影响普通文本背景
+  vim.g.neovide_hide_mouse_when_typing = true  -- 打字时，隐藏鼠标
+  vim.g.neovide_refresh_rate = 144  -- 刷新率
+  vim.g.neovide_refresh_rate_idle = 5  -- 空闲刷新率
+  vim.g.neovide_fullscreen = false  -- 全屏 end
+  vim.g.neovide_profiler = false  -- 左上角会显示一个小的帧数图
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,3 +1,7 @@
+if vim.g.neovide then
+  return {} -- 如果是 Neovide，则不加载该插件
+end
+
 return {
   "tokyonight.nvim",
   opts = {
